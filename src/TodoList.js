612693f2821811f0c42f1563d53e00e0427cb0c7
Brandon <beyu94@gmail.com>
2019-05-24 00:08:48 -0700

    rendered todolistform as textbox

---
+++ src/TodoList.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import TodoListForm from "./TodoListForm";
+
+export default class TodoList extends React.Component {
+    render(){
+        return <div><TodoListForm /></div>
+    }
+}
---
+++ src/TodoListForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+
+export default class TodoListForm extends React.Component {
+    state = {
+        text : ''
+    }
+    render(){
+        return <input />;
+    }
+}
